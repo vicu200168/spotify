82d92959b70a87a929649a09b04c9628f36ef9b5
Tu Nombre <vicugiannoni@gmail.com>
2024-11-12 20:23:08 -0300

    Correccion en imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
+import { UsuariosController } from './users/users.controller';
+import { UsuariosService } from './users/users.service';
 import { MusicService } from './music/music.service';
 import { MusicController } from './music/music.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsersController, MusicController],
-  providers: [AppService, UsersService, MusicService],
+  controllers: [AppController, UsuariosController, MusicController],
+  providers: [AppService, UsuariosService, MusicService],
 })
 export class AppModule {}
